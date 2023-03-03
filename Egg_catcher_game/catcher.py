d8065eea11236a70bc3ba2261280ff9a50a87ba1
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-03 16:33:05 +0530

    Finish the catcher

---
+++ Egg_catcher_game/catcher.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Catcher(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color('cyan')
+        self.speed('fastest')
+        self.shape('triangle')
+        self.right(90)
+        self.penup()
+        self.shapesize(stretch_wid=2, stretch_len=1)
+        self.speed('fastest')
+        self.goto(0, -280)
+
+    def go_left(self):
+        self.setx(self.xcor() - 10)
+
+    def go_right(self):
+        self.setx(self.xcor() + 10)
